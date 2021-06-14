f1b8aa44d2e90405ff19b23993b028f028bdb7e6
Shikyo <dennan_lol@hotmail.com>
2021-06-14 11:54:08 +0200

    Mutliple dimension arrays

--- LexiconLectureCollectionApp/Program.cs
+++ LexiconLectureCollectionApp/Program.cs
@@ -7,11 +7,50 @@ namespace LexiconLectureCollectionApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-           // Console.WriteLine("Int was: " + GetIntFromUser());
-            Console.WriteLine("Double was: " + GetDoubleFromUser());
+            // Console.WriteLine("Int was: " + GetIntFromUser());
+            //Console.WriteLine("Double was: " + GetDoubleFromUser());
 
-
+            MultiTable();
         } // End of main method
+
+        static void MultiTable()
+        {
+            //2D array
+            // GetLength                    0   1
+            int[,] multi_10_table = new int[10, 10];
+
+            //mutli_10_table.length gets you the entire size of the array which is 100
+            //We want the size of each dimension so getLength(0) where 0 is the first dimension
+            for (int y = 0; y < multi_10_table.GetLength(0); y++)
+            {
+                //Multi dimensional arrays are symmetrical at start so you can't array[j].length to get the length of the array inside the array
+                //multi_10_table.GetLength(1) here is the second dimension
+                for (int x = 0; x < multi_10_table.GetLength(1); x++)
+                {
+                    multi_10_table[y, x] = (y + 1) * (x + 1);
+                    Console.Write(multi_10_table[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+            //for (int y = 0; y < multi_10_table.GetLength(0); y++)
+            //{
+            //    for (int x = 0; x < multi_10_table.GetLength(1); x++)
+            //    {
+            //        Console.Write(multi_10_table[y, x] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //If we used '' instead of "" the program would convert the inside to its ascii value thus adding to the number
+            //So '\t' ascii is 9 thus number becomes 9 instead of 0 and no tabs are made
+            //foreach (int number in multi_10_table)
+            //{
+            //    Console.Write(number + "\t");
+            //}
+        }
 
         static int GetIntFromUser()
         {
@@ -20,6 +59,8 @@ namespace LexiconLectureCollectionApp
 
             do
             {
+                //Only catch exceptions you can do something about, exceptions you can't do something about might be catched somewhere else
+                //were it can be fixed
                 try
                 {
                     number = int.Parse(Console.ReadLine());
